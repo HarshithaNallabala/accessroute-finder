7af3cc15f54f4fd9e6af865e238a04b846642a9d
HarshithaNallabala <harshithanallabala@gmail.com>
2026-04-26 21:32:46 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,4 +3,4 @@
  * Change API_URL to match your Flask backend address.
  */
 export const API_URL = "http://127.0.0.1:5000";
-export const ROUTE_ENDPOINT = `${API_URL}/get-route`;
+export const ROUTE_ENDPOINT = `https://accessroute-finder.onrender.com`;
